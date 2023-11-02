72e3d995dd949a0093a0b93afdbdd4153e930016
miz <mizerablepi@gmail.com>
2023-11-02 23:15:27 +0530

    make navbar and set index route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
-import "./App.css";
+import logo from "./assets/waldo_face.jpg";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <header className="flex items-center justify-between shadow-md p-2 ">
+        <div className="flex items-center">
+          <img src={logo} alt="logo image" className="h-16" />
+          <h1 className="text-2xl mr-4 pr-4 border-r-2">
+            <span className="text-blue-600 font-bold">Where&apos;s </span>
+            <span className="text-red-600 font-bold"> Waldo?</span>
+          </h1>
+          <button className="p-2 m-1 text-red-600 font-bold self-stretch hover:bg-gray-200">
+            Home
+          </button>
+          <button className="p-1 m-1 self-stretch hover:bg-gray-200">
+            Play
+          </button>
+          <button className="p-1 m-1 self-stretch hover:bg-gray-200">
+            Leaderboard
+          </button>
+        </div>
+        <div className="">
+          <button className="px-2 py-1 m-1 bg-red-600 text-white font-bold rounded-lg">
+            Log In
+          </button>
+        </div>
+      </header>
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Home from "./Home.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+    // errorElement: <Errorpage/>
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
