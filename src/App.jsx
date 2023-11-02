78276bb044602a6a08291b8509facb6f8d93e619
miz <mizerablepi@gmail.com>
2023-11-02 23:33:24 +0530

    tidy up header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import logo from "./assets/waldo_face.jpg";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link, useLocation } from "react-router-dom";
 
 function App() {
+  const currentPage = useLocation().pathname;
   return (
     <>
       <header className="flex items-center justify-between shadow-md p-2 ">
@@ -11,18 +12,36 @@ function App() {
             <span className="text-blue-600 font-bold">Where&apos;s </span>
             <span className="text-red-600 font-bold"> Waldo?</span>
           </h1>
-          <button className="p-2 m-1 text-red-600 font-bold self-stretch hover:bg-gray-200">
-            Home
-          </button>
-          <button className="p-1 m-1 self-stretch hover:bg-gray-200">
-            Play
-          </button>
-          <button className="p-1 m-1 self-stretch hover:bg-gray-200">
-            Leaderboard
-          </button>
+          <Link to={""}>
+            <button
+              className={`p-2 m-1 ${
+                currentPage == "/" ? "text-red-600 font-bold" : ""
+              } self-stretch hover:bg-gray-200`}
+            >
+              Home
+            </button>
+          </Link>
+          <Link to={"play"}>
+            <button
+              className={`p-1 m-1 ${
+                currentPage == "/play" ? "text-red-600 font-bold" : ""
+              } self-stretch hover:bg-gray-200`}
+            >
+              Play
+            </button>
+          </Link>
+          <Link to={"leaderboard"}>
+            <button
+              className={`p-1 m-1 ${
+                currentPage == "/leaderboard" ? "text-red-600 font-bold" : ""
+              } self-stretch hover:bg-gray-200`}
+            >
+              Leaderboard
+            </button>
+          </Link>
         </div>
         <div className="">
-          <button className="px-2 py-1 m-1 bg-red-600 text-white font-bold rounded-lg">
+          <button className="px-2 py-1 m-1 bg-red-600 text-white font-bold rounded-lg hover:bg-red-700">
             Log In
           </button>
         </div>
