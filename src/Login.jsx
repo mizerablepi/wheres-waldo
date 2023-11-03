e8ffd1672c72ea954f6c88eb24d871bdbc13f254
miz <mizerablepi@gmail.com>
2023-11-04 01:29:30 +0530

    create login page

---
+++ src/Login.jsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <div className="flex flex-col items-center p-4">
+      <h2 className="font-bold text-2xl">Login to your account: </h2>
+      <form action="#" method="post" className="flex flex-col w-[30rem] mt-8">
+        <label htmlFor="username">Username: </label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          required
+          className="border-2 mb-4 border-red-600 focus-visible:outline-none focus-visible:border-red-800"
+        />
+        <label htmlFor="password">Password: </label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          required
+          className="border-2 mb-4 border-red-600 focus-visible:outline-none focus-visible:border-red-800"
+        />
+        <button className="bg-blue-600 self-start py-1 px-3 rounded-md text-white font-bold my-2">
+          Login
+        </button>
+        <span className="self-start">
+          No account?{" "}
+          <Link to={"/signup"} className="underline text-sky-600">
+            Sign Up
+          </Link>
+        </span>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
