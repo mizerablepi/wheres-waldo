2d63e75efb518810a1f5e97f6660f19f0acea8cd
miz <mizerablepi@gmail.com>
2023-11-12 21:44:33 +0530

    implment the play page

--- client/src/Game.jsx
+++ client/src/Game.jsx
@@ -1,7 +1,14 @@
+import MapCard from "./MapCard";
+import laundryMap from "./assets/laundryMap.jpg";
+import spaceMap from "./assets/spaceMap.jpg";
+
 export default function Game() {
   return (
-    <>
-      <h1>YOU WANNA PLAY LETS PLAY</h1>
-    </>
+    <div id="play" className="flex min-h-[calc(100vh-80px)]">
+      <div className="bg-[rgba(0,0,0,0.7)] w-full flex justify-center items-start gap-8 p-10 ">
+        <MapCard imageSrc={laundryMap} mapName={"Laundry"} />
+        <MapCard imageSrc={spaceMap} mapName={"Space"} />
+      </div>
+    </div>
   );
 }
