36e4a176f7c05f823c39eacea96391db1d2ec1fb
miz <mizerablepi@gmail.com>
2023-11-20 00:31:54 +0530

    add rotes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const controller = require("../controllers/index.controller");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
+router.get("/play/:map", controller.getCharacterList);
+router.post("/play/:map/check", controller.checkCoords);
+router.get("/leaderboard", controller.getLeaderboard);
+router.get("/startTime", controller.startTime);
+router.post("/submit/:map", controller.addScore);
 
 module.exports = router;
