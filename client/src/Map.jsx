d97c2a794ec05a909bf07ee11c76513b848b3c53
miz <mizerablepi@gmail.com>
2023-11-20 12:20:51 +0530

    extract modal to new component

--- client/src/Map.jsx
+++ client/src/Map.jsx
@@ -2,10 +2,10 @@ import { useParams } from "react-router-dom";
 import space from "./assets/spaceMap.jpg";
 import laundry from "./assets/laundryMap.jpg";
 import { useEffect, useRef, useState } from "react";
-import { redirect } from "react-router-dom";
 import ChoiceMenu from "./ChoiceMenu";
 import Stopwatch from "./Stopwatch";
 import { getCharacterList } from "./utility";
+import UserModal from "./UsernameModal";
 
 const Map = () => {
   const [clicked, setClicked] = useState(false);
@@ -35,49 +35,7 @@ const Map = () => {
   return (
     list[0] != "Loading data..." && (
       <div>
-        <div
-          className={`fixed w-screen h-screen flex flex-col justify-center items-center z-20 top-0 bg-black/50 ${
-            score > 0 ? "block" : "hidden"
-          }`}
-        >
-          <div className="font-bold text-white">
-            Your Score is{" "}
-            {Math.floor((score / 60000) % 60)
-              .toString()
-              .padStart(2, "0")}
-            :
-            {Math.floor((score / 1000) % 60)
-              .toString()
-              .padStart(2, "0")}
-            :
-            {Math.round((score % 1000) / 10)
-              .toString()
-              .padStart(2, "0")}
-          </div>
-          <form
-            action={"http://localhost:3000/submit/" + name}
-            className="flex flex-col mt-4"
-            method="post"
-            onSubmit={() => {
-              redirect("/");
-            }}
-          >
-            <input
-              type="text"
-              required
-              minLength={1}
-              placeholder="TEST"
-              name="username"
-              className="p-2 border-red-600 border"
-            />
-            <button
-              type="submit"
-              className="text-white font-bold bg-blue-600 mt-2 self-center px-4 py-2 rounded"
-            >
-              SUBMIT
-            </button>
-          </form>
-        </div>
+        <UserModal score={score} />
         <Stopwatch
           clockStarted={clockStarted}
           charactersFound={charactersFound}
