fbf06e45b42010071df36ba2f02116b3ececf0a2
miz <mizerablepi@gmail.com>
2023-11-23 00:28:07 +0530

    asd

--- server/controllers/index.controller.js
+++ server/controllers/index.controller.js
@@ -15,7 +15,7 @@ exports.getLeaderboard = asyncHandler(async (req, res) => {
   const scores = await Scores.find().sort({ time: 1 }).limit(5).exec();
   console.log(req.session.id);
   res.cookie("name", "slimshady", {
-    sameSite: "None",
+    sameSite: "none",
     secure: true,
   });
   res.json(scores);
