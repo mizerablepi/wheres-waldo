14e89bd91d8e4a27aeca1d27939b50b139eb7fa0
miz <mizerablepi@gmail.com>
2023-11-23 00:56:59 +0530

    test6

--- server/app.js
+++ server/app.js
@@ -38,11 +38,9 @@ app.use(
     resave: false,
     saveUninitialized: true,
     secure: true,
-    httpOnly: true,
     cookie: {
       sameSite: "none", // THIS is the config you are looking for.
       secure: true,
-      httpOnly: true,
     },
   })
 );
